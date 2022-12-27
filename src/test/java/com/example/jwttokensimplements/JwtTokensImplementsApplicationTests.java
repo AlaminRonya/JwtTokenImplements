b38999de26f6982a83a07d1cbc214355ce7096a9
AlaminRonya <hprony12@gmail.com>
2022-12-27 15:53:03 +0600

    Jwt tokens project

---
+++ src/test/java/com/example/jwttokensimplements/JwtTokensImplementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwttokensimplements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokensImplementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
