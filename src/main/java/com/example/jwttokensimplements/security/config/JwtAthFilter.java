4a88898a0cd3632a1543626086758601f11e2cf8
AlaminRonya <hprony12@gmail.com>
2022-12-28 22:54:20 +0600

    implement UserDetailsService

--- src/main/java/com/example/jwttokensimplements/security/config/JwtAthFilter.java
+++ src/main/java/com/example/jwttokensimplements/security/config/JwtAthFilter.java
@@ -1,10 +1,9 @@
-package com.example.jwttokensimplements.config;
+package com.example.jwttokensimplements.security.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/com/example/jwttokensimplements/security/config/JwtUtils.java
+++ src/main/java/com/example/jwttokensimplements/security/config/JwtUtils.java
@@ -1,4 +1,4 @@
-package com.example.jwttokensimplements.config;
+package com.example.jwttokensimplements.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
