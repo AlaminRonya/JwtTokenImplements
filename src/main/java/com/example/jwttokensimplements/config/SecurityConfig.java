6e0dba58e6405b45f58978412b0130c8db748b1c
AlaminRonya <hprony12@gmail.com>
2022-12-28 22:34:20 +0600

    use to jwt filter class

--- src/main/java/com/example/jwttokensimplements/config/SecurityConfig.java
+++ src/main/java/com/example/jwttokensimplements/config/SecurityConfig.java
@@ -1,19 +1,25 @@
 package com.example.jwttokensimplements.config;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
