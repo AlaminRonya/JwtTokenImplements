dfb523739b190b7e20fa096855de1d3eaf0f03a8
AlaminRonya <hprony12@gmail.com>
2022-12-28 21:26:04 +0600

    implement jwt utils class

--- src/main/java/com/example/jwttokensimplements/config/JwtAthFilter.java
+++ src/main/java/com/example/jwttokensimplements/config/JwtAthFilter.java
@@ -4,24 +4,30 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAthFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
 
-    public JwtAthFilter(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+//    public JwtAthFilter(UserDetailsService userDetailsService, JwtUtils jwtUtils) {
+//        this.userDetailsService = userDetailsService;
+//        this.jwtUtils = jwtUtils;
+//    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -33,11 +39,11 @@ public class JwtAthFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authHeader.substring(7);
-        userEmail = "user@gmail.com"; // TODO: 12/27/2022 get userEmail implement
+        userEmail = jwtUtils.extractUsername(jwtToken); // TODO: 12/27/2022 get userEmail implement
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-            final boolean isTokenValid = false; // TODO: 12/27/2022 check validation implement
-            if (isTokenValid){
+//            final boolean isTokenValid = false; // TODO: 12/27/2022 check validation implement
+            if (jwtUtils.isTokenValid(jwtToken, userDetails)){
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
